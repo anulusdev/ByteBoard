6a29fa01a3d48adf4514ac1df7c79e016b440c96
AnulusDev <khalidalhazan@gmail.com>
2025-10-10 22:56:23 +0100

    admin user

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-
 python manage.py migrate
 
 python manage.py loaddata posts
 
-if [ "$CREATE_SUPERUSER" = "True" ]:
-  python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
+
+python manage.py collectstatic --no-input
